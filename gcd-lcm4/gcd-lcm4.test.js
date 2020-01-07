7d267188fb7b79ac56028ec1e01e416146349b22
g Yim <gyim1345@gmail.com>
2020-01-07 09:44:45 +0900

    Add gcd-lcm4.test.js, readme.md
    Add js and readme file about gcd-lcm algorithm problem

---
+++ gcd-lcm4/gcd-lcm4.test.js
@@ -0,0 +1,28 @@
+const solution = (n, m) => {
+    return [gcd(n,m), n*m/gcd(n,m)]
+ }
+
+const gcd = (n,m) => {
+    return Math.max(...commonDivisor(n).filter(x => x = commonDivisor(m).includes(x)))
+}
+
+const commonDivisor = (n) => {
+    return Array(n).fill().map( (_,i) => n = i+1).filter(x => n % x === 0)
+}
+
+
+
+
+// test('gcd', () => {
+//     expect(gcd(72, 192)).toBe(24);
+// });
+
+
+// test('commonDivisor', () =>{
+//     expect(commonDivisor(72)).toEqual([1, 2, 3, 4, 6, 8, 9, 12, 18, 24, 36, 72])
+// })
+
+test('solution', () => {
+    // expect(solution(3,12)).toEqual([3,12])
+    expect(solution(72,192)).toEqual([24,576])
+})
