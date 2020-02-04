1152b666e54281a7d6e83c4ad6beb820266f2e03
gyim <gyim1345@gmail.com>
2020-02-04 09:25:48 +0900

    Finish problem failureRate2

--- failureRate2/failureRate2.test.js
+++ failureRate2/failureRate2.test.js
@@ -1,22 +1,19 @@
 const failureRate = (N,stages) => {
+    
     const currentPeopleOnStage =  Array(N).fill(0)
     .map((x,i) => x = stages.filter(v => v === i+1).length)
-    //incomplete 
+
     const result = 
-        Array(N).fill(0)
-        .map((x,i) => x = stages.filter(v => v === i+1).length)
-        .map((x,i) => (x)/(stages.length- currentPeopleOnStage.reduce((a, b) => a + b, 0)) === Infinity ? 0 : x/(stages.length- currentPeopleOnStage.reduce((a, b) => a + b, 0))) 
-        .map(v => isNaN(v) ? 0 : v)
+    currentPeopleOnStage.map((x,i) => (x)/(stages.length- currentPeopleOnStage.slice(0,i).reduce((a, b) => a + b, 0)) === Infinity ? 0 : x/(stages.length-currentPeopleOnStage.slice(0,i).reduce((a, b) => a + b, 0)))
+        //Infinity 처리 해줄 필요 없음. 지워도 됨.
+        .map(v => isNaN(v) ? 0 : v)//이건 필요하다
         .map((v,i)=> [v,i])
         .sort(function(a, b) {
             return (a[0] === b[0] ? a[1]-b[1] : b[0] - a[0]);
         })
         .map((v,i )=> v[1]+1)    
-    
-        // stages.forEach(x => result[x-1]= result[x-1]+1);
-    console.log(result)
 
-    return [3,4,2,1,5];
+    return result;
 }
 
 test( 'failureRate' , () => {
