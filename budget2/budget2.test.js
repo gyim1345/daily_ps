75cb02cbd7363bb6b26deb34a2a8e009898814d2
gyim <gyim1345@gmail.com>
2020-02-26 11:33:27 +0900

    Refactor

--- budget2/budget2.test.js
+++ budget2/budget2.test.js
@@ -1,12 +1,30 @@
-const budgetf = (d, budget, count = 0) => {
-    d.sort((a,b)=> {return a-b})
+const budgetf = (d, budget) => {
+    d.sort((a,b)=> a-b);
 
-    if(budget - d[count]<0 || count >= d.length)
-    return count
-    
-    return budgetf(d, budget - d[count], count+1)
- 
+    return step(d, budget, 0);
 }
+
+const step = (d, budget, count) => {
+    if(budget - d[count] < 0 || count >= d.length) {
+        return count;
+    }
+    
+    return step(d, budget - d[count], count + 1)
+};
+
+// const budgetf = (d, budget, count = 0) => {
+//     if( count === 0 )
+//       d.sort((a,b)=> {return a-b})
+
+//     if(budget - d[count]<0 || count >= d.length)
+//     return count
+    
+//     return budgetf(d, budget - d[count], count+1)
+ 
+// }
+
+
+
 
 
 test('budget', () => {
