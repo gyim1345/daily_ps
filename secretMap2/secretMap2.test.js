3be14ba32679c53050cf6b3799ec92fe544a0af5
gyim <gyim1345@gmail.com>
2020-04-09 11:32:03 +0900

    Add some extra content

--- secretMap2/secretMap2.test.js
+++ secretMap2/secretMap2.test.js
@@ -5,7 +5,7 @@ const secretMap = (n, arr1, arr2) =>
         .padStart(n,0)
         .replace(/0/g,' ')
         .replace(/1/g,'#'))
-
+//replace(/1|0/g, a => +a ? '#' : ' ')) 로 쓸 수 도 있다.
 test('secretMap', () => {
     expect(secretMap(5, [9, 20, 28, 18, 11], [30, 1, 21, 17, 28])).toEqual(["#####", "# # #", "### #", "#  ##", "#####"]);
 });
