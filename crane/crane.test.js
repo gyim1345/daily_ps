3afeea605870f10e096bb20dccc930a453e4ef65
gyim <gyim1345@gmail.com>
2020-04-10 11:22:29 +0900

    Finish up crane problem

--- crane/crane.test.js
+++ crane/crane.test.js
@@ -1,45 +1,53 @@
 const crane = (board, moves) => {
     const array = [];
     let count = 0;
-    let y =0;
-    while(moves.length>0){
-        console.log(board, moves)
-
-        if(board[y][moves[0]-1] !== 0 ){
-            array.push(board[y][moves[0]-1]);
-            board[y][moves[0]-1] =0;
+    let y = 0;
+    while (moves.length > 0) {
+        if (board[y][moves[0] - 1] !== 0) {
+            array.push(board[y][moves[0] - 1]);
+            board[y][moves[0] - 1] = 0;
             moves.shift();
             y = 0;
         }
         else {
-        y += 1;
-        if(y === board.length){
-            y = 0;
-            moves.shift();
-        }
-
+            y += 1;
+            if (y === board.length) {
+                y = 0;
+                moves.shift();
+            }
         }
         count += 1;
     }
 
-    
-    let count1 =0;
-    let pairCount =0;
-    let Length = array.length
-    while(count1 <Length) {
-        if(array[count1] === array[count1+1]) {
-            pairCount += 1;
-            array.splice(count1,2);
-            Length = array.length
-            console.log(array)
-            count1=0;
-        }
-        count1 += 1;
-    }
-
-    return pairCount*2;
+    return countPair(array) * 2;
 }
 
-test('crane', ()=> {
-    expect(crane([[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]],[1,5,3,5,1,2,1,4])).toBe(4)
+const countPair = (array) => {
+    let pairCount = 0;
+    let modified = true;
+    while (modified) {
+        modified = false;
+        for (let i = 0; i < array.length; i += 1) {
+            if (array[i] === array[i + 1]) {
+                pairCount += 1;
+                array.splice(i, 2);
+                modified = true;
+            }
+        }
+    }
+    return pairCount
+}
+
+test('countPair', () => {
+    expect(countPair([4, 3, 2, 1, 1, 2, 3, 4])).toBe(4)
+    expect(countPair([1, 1, 2, 2])).toBe(2)
+    expect(countPair([1,4,6,8,8,6,3,2,6,6,8,8,6])).toBe(4)
+})
+
+test('crane', () => {
+    // expect(crane([[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]],[1,5,3,5,1,2,1,4])).toBe(4)
+    // expect(crane([[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]],[])).toBe(0)
+    expect(crane([[0, 0, 0, 0, 0], [0, 0, 1, 0, 3], [0, 2, 5, 0, 1], [4, 2, 4, 4, 2], [3, 5, 1, 3, 1]], [1])).toBe(0)
+
+
 })
