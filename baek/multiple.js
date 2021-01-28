977b609429f21f91305e0912d489e8a6dc3d43a1
gyim1345 <gyim1345@gmail.com>
2021-01-28 17:51:10 +0900

    solve problem using JavaScript

---
+++ baek/multiple.js
@@ -0,0 +1,33 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+  input.push(line)
+})
+  .on('close', function () {
+    const numbers = [1,2,3,4,5,6,7,8,9]
+    numbers.forEach(number => {
+        console.log(`${input[0]} * ${number} = ${number * Number(input[0])}`);
+    })
+  process.exit();
+});
+
+//2
+
+/*
+2 * 1 = 2
+2 * 2 = 4
+2 * 3 = 6
+2 * 4 = 8
+2 * 5 = 10
+2 * 6 = 12
+2 * 7 = 14
+2 * 8 = 16
+2 * 9 = 18
+*/
